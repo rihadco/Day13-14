6da6ac5df9e20d9bdd974692c1c5eb092afd9d02
rihadco <rihad.covcic@gmail.com>
2021-03-21 21:17:23 +0100

    Vjezba 13-14

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,25 +1,42 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+let slideIndex = 1;
+showSlide(slideIndex);
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+function openLightbox() {
+    document.getElementById('Lightbox').style.display = 'block';
+};
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+function closeLightbox() {
+    document.getElementById('Lightbox').style.display = 'none';
+};
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+function toSlide(n) {
+    showSlide(slideIndex = n);
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+function showSlide(n) {
+    const slides = document.getElementsByClassName('slide');
+    let modalPreviews = document.getElementsByClassName('modal-preview');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    };
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    };
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    };
+
+    for (let i = 0; i < modalPreviews.length; i++) {
+        modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+    };
+
+    slides[slideIndex - 1].style.display = 'block';
+    modalPreviews[slideIndex - 1].className += ' active';
+};
